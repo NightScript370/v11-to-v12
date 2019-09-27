f1b344980e410fd421556fa6f6eb4ccc3d2dac07
NightYoshi370 <maorninja@outlook.com>
2019-09-26 21:16:35 -0700

    GuildMember is done
    except sendFile but the person I'm making this for doesn't use that either way

--- GuildMember.js
+++ GuildMember.js
@@ -3,11 +3,52 @@ module.exports = Structures.extend("GuildMember", GuildMember => class LazyV12Gu
         super(...args)
     }
 
-    addRole (id, reason) {
-        return this.roles.add(id, reason);
+    addRole (id, reason='') {
+        return this.roles.add(id, reason='');
     }
 
-    addRoles (id, reason) {
-        return this.roles.add(id, reason);
+    addRoles (id, reason='') {
+        return this.roles.add(id, reason='');
+    }
+
+    removeRole (id, reason='') {
+        return this.roles.remove(id, reason='');
+    }
+
+    removeRoles (id, reason='') {
+        return this.roles.remove(id, reason='');
+    }
+
+    setRoles (id, reason='') {
+        return this.roles.set(id, reason='');
+    }
+
+    hasPermission (perm, _, checkAdmin=true, checkOwner=true) {
+        return GuildMember.hasPermission(perm, {checkAdmin, checkOwner})
+    }
+
+    hasPermissions (perm, _) {
+        return GuildMember.hasPermission(perm)
+    }
+
+    sendCode(lang, code, options={}) {
+        return this.send("```" + lang + "\n" + code + "```", options);
+    }
+
+    sendEmbed(embed, messageContent="", options={}) {
+        options.embed = embed;
+        return this.send(messageContent, options);
+    }
+
+    sendMessage(messageContent="", options={}) {
+        return this.send(messageContent, options);
+    }
+
+    setDeaf(deaf, reason="") {
+        return this.voice.setDeaf(deaf, reason);
+    }
+
+    setMute(mute, reason="") {
+        return this.voice.setMute(mute, reason);
     }
 })
